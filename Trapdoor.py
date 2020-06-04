40a1ac132b12297af38b921797dd2746c0761450
parik <parikshitsam@gmail.com>
2020-06-05 00:01:26 +0530

    making a comprehensive ras encryption function

--- Trapdoor.py
+++ Trapdoor.py
@@ -1,4 +1,65 @@
+def alt_even_nums(nums):
+    lst = []
+    for num in range(nums):
+        if num%2 == 0:
+            lst.append(num)
+    return lst
+
+def alt_odd_nums(nums):
+    lst = []
+    for num in range(nums):
+        if num%2 != 0:
+            lst.append(num)
+    return lst
+
+def alt_char_of_str(password):
+    lst = []
+    if len(password)%2 == 0:
+        even_nums = alt_even_nums(len(password))
+        for num in even_nums:
+            first_char = password[num]
+            second_char = password[num+1]
+            lst.append(first_char+second_char)
+        return lst
+    else:
+        odd_nums = alt_odd_nums(len(password))
+        for num in odd_nums:
+            first_char = password[num-1]
+            second_char = password[num]
+            lst.append(first_char+second_char)
+        lst.append(password[-1])
+        return lst
+#print(alt_char_of_str("abcdefghi"))
+
+
+
+def encrypt2(password):
+    alphabets_symbols = """abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+-={\}[]';/";:/?.>,<"""
+    p = 7523
+    q = 5843
+    n = p*q
+    e = 6029
+    crypt = ""
+    block_length_plaintext = 2
+    block_length_crypttext = 15
+    lst_char_pass = alt_char_of_str(password)
+    crypt_nums = []
+
+    for str in lst_char_pass:
+        if len(str)==2:
+            a = alphabets_symbols.index(str[0])
+            b = alphabets_symbols.index(str[1])
+            crypt_nums.append((len(alphabets_symbols)*a)+b)
+        else:
+            crypt_nums.append(alphabets_symbols.index(str))
+    return crypt_nums
 p = 7523
 q = 5843
-n= p*q
-print(n)
+#n = p*q
+e = 6029
+block_length_plaintext = 2
+block_length_crypttext = 15
+#print((66**block_length_plaintext)<n)
+print(encrypt2("abcdefgh"))
+
+#def decrypt2(password)
